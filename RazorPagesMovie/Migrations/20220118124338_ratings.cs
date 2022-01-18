b4e63e0c66a467b198468678213810735ff32c03
Amit Korgaonkar <amitcor12@gmail.com>
2022-01-18 18:26:31 +0530

    Search and Update

---
+++ RazorPagesMovie/Migrations/20220118124338_ratings.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class ratings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "Decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "Decimal(18,2)");
+        }
+    }
+}
--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -20,10 +21,29 @@ namespace RazorPagesMovie.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString {get;set;}
+        public SelectList Gernes{get;set;}
+        [BindProperty(SupportsGet = true)]
+        public string MovieGerne{get;set;}
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> gerneQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+            
+            
+
+            var movie = from m in _context.Movie select m;
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                movie = movie.Where(s=>s.Title.Contains(SearchString));
+            }
+            if(!string.IsNullOrEmpty(MovieGerne))
+            {
+                movie = movie.Where(x=>x.Genre == MovieGerne);
+            }
+            Gernes = new SelectList(await gerneQuery.Distinct().ToListAsync());
+            Movie = await movie.ToListAsync();
         }
     }
 }
